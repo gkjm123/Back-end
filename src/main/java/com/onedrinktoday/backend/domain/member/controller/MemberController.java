6786de03b653ee15c4cc192e87827ea776f99b74
baby <gkje123@naver.com>
2024-09-04 22:42:04 +0900

    refactor: Jwt 인증 기능 수정
    - TokenDTO -> TokenDto 이름 변경
    - JwtFilter: 토큰 유효성 검증 -> 실패시 exception 별 응답 리턴
    - JwtProvider: isTokenExpired() 삭제 -> getEmail() 실행시 유효성 검증 포함(실패시 Exception)
    - GoogleService: 구글 로그인시 TokenDto 반환
    - MemberService: getMember() 생성
    - MemberService: refreshAccessToken() 시 토큰 둘다 새로 발급됨 -> access 토큰만 재발급 되도록 수정

--- src/main/java/com/onedrinktoday/backend/domain/member/controller/MemberController.java
+++ src/main/java/com/onedrinktoday/backend/domain/member/controller/MemberController.java
@@ -7,7 +7,6 @@ import com.onedrinktoday.backend.global.security.TokenDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
