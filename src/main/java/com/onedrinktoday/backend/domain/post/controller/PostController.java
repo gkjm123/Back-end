4645523d7b5407cf7d23fdb9e6f7775fa82265c6
BigHuni <hdh8659@gmail.com>
2024-09-09 07:28:41 +0900

    feat: Post 특정 게시글 조회 구현

--- src/main/java/com/onedrinktoday/backend/domain/post/controller/PostController.java
+++ src/main/java/com/onedrinktoday/backend/domain/post/controller/PostController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -29,5 +30,11 @@ public class PostController {
   public ResponseEntity<List<PostResponse>> getAllPosts() {
     List<PostResponse> posts = postService.getAllPosts();
     return ResponseEntity.ok(posts);
+  }
+
+  @GetMapping("/posts/{postId}")
+  public ResponseEntity<PostResponse> getPostById(@PathVariable Long postId) {
+    PostResponse postResponse = postService.getPostById(postId);
+    return ResponseEntity.ok(postResponse);
   }
 }
