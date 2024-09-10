d760a0e20e85196fc5583cb44118b1450c22c015
baby <gkje123@naver.com>
2024-09-10 13:15:35 +0900

    refactor: Manager 역할만 승인 가능하도록 수정

--- src/main/java/com/onedrinktoday/backend/domain/manager/controller/ManagerController.java
+++ src/main/java/com/onedrinktoday/backend/domain/manager/controller/ManagerController.java
@@ -4,6 +4,7 @@ import com.onedrinktoday.backend.domain.drink.dto.DrinkResponse;
 import com.onedrinktoday.backend.domain.manager.service.ManagerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +17,14 @@ public class ManagerController {
 
   private final ManagerService managerService;
 
+  @PreAuthorize("hasRole('MANAGER')")
   @PutMapping("/manager/registrations/{registId}/approve")
   public ResponseEntity<DrinkResponse> approveRegistration(@PathVariable Long registId) {
 
     return ResponseEntity.ok(managerService.approveRegistration(registId));
   }
 
+  @PreAuthorize("hasRole('MANAGER')")
   @PutMapping("/manager/registrations/{registId}/cancel")
   public ResponseEntity<String> cancelRegistration(@PathVariable Long registId) {
 
