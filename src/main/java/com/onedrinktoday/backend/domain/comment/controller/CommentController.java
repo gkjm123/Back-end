f6530868eab104627fff39a59ff1d9fddec90ed5
Hyun-jun-Lee0811 <korong0811@naver.com>
2024-09-10 19:29:45 +0900

    refactor: 유효성 검증과 페이징 리팩토리

--- src/main/java/com/onedrinktoday/backend/domain/comment/controller/CommentController.java
+++ src/main/java/com/onedrinktoday/backend/domain/comment/controller/CommentController.java
@@ -3,10 +3,12 @@ package com.onedrinktoday.backend.domain.comment.controller;
 import com.onedrinktoday.backend.domain.comment.dto.CommentRequest;
 import com.onedrinktoday.backend.domain.comment.dto.CommentResponse;
 import com.onedrinktoday.backend.domain.comment.service.CommentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,7 +27,8 @@ public class CommentController {
   private final CommentService commentService;
 
   @PostMapping("/comments")
-  public ResponseEntity<CommentResponse> createComment(@RequestBody CommentRequest commentRequest) {
+  public ResponseEntity<CommentResponse> createComment(
+      @Valid @RequestBody CommentRequest commentRequest) {
 
     CommentResponse commentResponse = commentService.createComment(commentRequest);
     return ResponseEntity.ok(commentResponse);
@@ -35,9 +37,8 @@ public class CommentController {
   @GetMapping("/{postId}/comments")
   public ResponseEntity<Page<CommentResponse>> getAllComments(
       @PathVariable Long postId,
-      @RequestParam(defaultValue = "0") int page,
-      @RequestParam(defaultValue = "10") int size) {
-    Pageable pageable = PageRequest.of(page, size);
+      @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+
     Page<CommentResponse> comments = commentService.getAllComments(postId, pageable);
     return ResponseEntity.ok(comments);
   }
@@ -52,6 +53,7 @@ public class CommentController {
   public ResponseEntity<CommentResponse> updateCommentById(
       @PathVariable Long commentId,
       @RequestBody CommentRequest commentRequest) {
+
     CommentResponse updatedComment = commentService.updateComment(commentId, commentRequest);
     return ResponseEntity.ok(updatedComment);
   }
