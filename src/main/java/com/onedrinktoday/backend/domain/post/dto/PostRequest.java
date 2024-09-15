e4b632d6488d817536dbb2cabf2804478eb7472b
Hyun-jun-Lee0811 <korong0811@naver.com>
2024-09-15 22:31:57 +0900

    refactor: 테스트에 필요한 필드 @Setter 추가

--- src/main/java/com/onedrinktoday/backend/domain/post/dto/PostRequest.java
+++ src/main/java/com/onedrinktoday/backend/domain/post/dto/PostRequest.java
@@ -7,23 +7,28 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostRequest {
+  @Setter
   private Long drinkId;
 
   private Long memberId;
 
   private Type type;
 
+  @Setter
   @Size(min = 10, max = 3000)
   private String content;
 
+  @Setter
   private Float rating;
 
+  @Setter
   private List<String> tag;
 
   private String imageUrl;
