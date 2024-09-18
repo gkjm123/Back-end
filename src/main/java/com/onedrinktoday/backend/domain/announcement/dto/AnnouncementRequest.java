e4b22682e4f470662d4751a6185afba699b15028
Hyun-jun-Lee0811 <korong0811@naver.com>
2024-09-19 01:26:56 +0900

    refactor: dto @Setter 추가

--- src/main/java/com/onedrinktoday/backend/domain/announcement/dto/AnnouncementRequest.java
+++ src/main/java/com/onedrinktoday/backend/domain/announcement/dto/AnnouncementRequest.java
@@ -5,15 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AnnouncementRequest {
+
   @Size(min = 2, max = 50)
   private String title;
+
   @Size(min = 10, max = 3000)
   private String content;
+
   private String imageUrl;
 }
